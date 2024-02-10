79ae4eae28d46b7c5cccb7f035e908f669f7eddc
alfiecg24 <alfie@alfiecg.uk>
2024-02-10 12:46:33 +0000

    Initial Commit

---
+++ TrollInstallerX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrollInstallerX
+//
+//  Created by Alfie on 10/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrollInstallerX/TrollInstallerXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrollInstallerXApp.swift
+//  TrollInstallerX
+//
+//  Created by Alfie on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrollInstallerXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
